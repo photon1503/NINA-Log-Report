15e88357a14e082c98283de9cd1045643a58d89c
Gerald Hitz <g.hitz@gmx.at>
2025-01-11 21:01:08 +0100

    sorting

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ TIME_DURATION_UNITS = (
     ('m', 60),
     ('s', 1)
 )
-
+def toDateTime(timestamp):
+    if not timestamp or timestamp == 0:
+        return None
+    if isinstance(timestamp, int):
+        return datetime.fromtimestamp(timestamp)
+    if isinstance(timestamp, str):
+        return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f')
+    raise ValueError("Unsupported timestamp format")
 
 def human_time_duration(seconds):
     if seconds == 0:
@@ -19,17 +26,18 @@ def human_time_duration(seconds):
             parts.append('{}{}{}'.format(amount, unit, "" ))
     return ' '.join(parts)
 
-def toDateTime(timestamp):
-    return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f')
+#def toDateTime(timestamp):
+#    return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f')
 
 def format_timestamp(timestamp):
     dt = toDateTime(timestamp)
     return dt.strftime('%H:%M')
 
-class Unsafe:
-    def __init__(self):
-        self.startTimestamp = 0
-        self.endTimestamp = 0
+class Event:
+    def __init__(self,eventType="",startTimestamp=0,endTimestamp=0):
+        self.startTimestamp = startTimestamp
+        self.endTimestamp = endTimestamp
+        self.eventType = eventType
 
     def minutes(self):
         return (toDateTime(self.endTimestamp) - toDateTime(self.startTimestamp)).seconds / 60
@@ -41,13 +49,14 @@ class Night:
         self.startTimestamp = 0
         self.endTimestamp = 0
         self.exposureTime = 0
-        self.unsafe = []
+        self.exposures = 0
+        self.events = []
 
     def updateStartEnd(self, timestamp):
         if self.startTimestamp == 0:
             self.startTimestamp = timestamp
         self.endTimestamp = timestamp
-
+   
     def getSequenceDuration(self):
         return (toDateTime(self.endSequence) - toDateTime(self.startSequence)).seconds 
     
@@ -56,8 +65,9 @@ class Night:
     
     def getTotalUnsafeMinutes(self):
         total = 0
-        for unsafe in self.unsafe:
-            total += unsafe.minutes()
+        for event in self.events:
+            if event.eventType == "unsafe":
+                total += event.minutes()
         return total
     
     def getTotalSafeMinutes(self):
@@ -85,6 +95,12 @@ class Object:
     def add(self, observation):
         self.observations.append(observation)
 
+    def getStartTimestamp(self):
+        return str(self.observations[0].date)
+    
+    def getEndTimestamp(self):
+        return str(self.observations[-1].date)
+
     def get_summary(self):
         total_exposure_time = sum(obs.exposure for obs in self.observations)
         average_hfr = sum(obs.hfr for obs in self.observations) / len(self.observations)
@@ -109,7 +125,9 @@ class Object:
         for filter, (total_exposure, num_exposures) in exposure_time_by_filter.items():
             print(f"  Filter {filter}: {num_exposures} exposures, {human_time_duration(total_exposure)} ")
 
-        print(f"Total {number_of_observations} exposures,  Exposure Time: {human_time_duration(total_exposure_time)}")
+        # if more then one filter
+        if len(exposure_time_by_filter) > 1:
+            print(f"Total {number_of_observations} exposures,  Exposure Time: {human_time_duration(total_exposure_time)}")
         print(f"Average HFR: {average_hfr} asec" )
         print(f"Average Drift: {average_drift}")
         
@@ -128,7 +146,10 @@ def log_parser(log_file):
     objects = {}
     obj = Observation()
     night = Night()
-    unsafe = Unsafe()
+    event = Event()
+
+    messageOld = ""
+    
 
     for line in lines:
         # if line starts with "---" then ignore it
@@ -148,26 +169,31 @@ def log_parser(log_file):
 
         if member == "StartSequence":
             if message.startswith("Advanced Sequence finished"):
-                night.endSequence = obj.date
+                if not messageOld.startswith("Sequence run was cancelled"):                        
+                    night.endSequence = obj.date
+                    night.events.append(Event("Sequence finished", 0, night.endSequence))
 
         if member == "StartSequence":
-            if message.startswith("Advanced Sequence starting"):
-                night.startSequence = obj.date
+            if message.startswith("Advanced Sequence starting"):                
+                if not messageOld.startswith("InterruptWhen: Sequence longer running"):                    
+                    night.startSequence = obj.date
+                    night.events.append(Event("Sequence starting", night.startSequence, 0))
 
         if member == "UpdateMonitorValues":
             if message.startswith("SafetyMonitorInfo state changed to Unsafe"):
-                unsafe = Unsafe()
-                unsafe.startTimestamp = obj.date
+                event = Event("unsafe")
+                event.startTimestamp = obj.date
+                obj = Observation()
         
         if member == "UpdateMonitorValues":
             if message.startswith("SafetyMonitorInfo state changed to Safe"):
                 
-                unsafe.endTimestamp = obj.date
-                night.unsafe.append(unsafe)
+                event.endTimestamp = obj.date
+                night.events.append(event)
 
         if member == "Capture":
             obj.exposure = int(message.split(";")[0].split(":")[1].replace("s", ""))
-            night.exposureTime += obj.exposure
+            
             night.updateStartEnd(obj.date)
 
         if member == "Detect":            
@@ -182,6 +208,10 @@ def log_parser(log_file):
                 continue
             obj.filter = message.split("\\")[-2]            
             obj.name = message.split("\\")[-4]
+            night.exposureTime += obj.exposure
+            night.exposures += 1
+           
+            
 
             
 
@@ -189,8 +219,10 @@ def log_parser(log_file):
                 objects[obj.name] = Object(obj.name)
             else:                
                 objects[obj.name].add(obj)
-
             obj = Observation()
+
+        messageOld = message
+        
            
     f.close()
 
@@ -199,12 +231,38 @@ def log_parser(log_file):
 
     print (f"Sequence started at {night.startSequence} and ended at {night.endSequence} ({human_time_duration(night.getSequenceDuration())})")
     print (f"Imaged from {night.startTimestamp} until {night.endTimestamp} ({human_time_duration(night.getImagingDuration())})")    
-    print (f"Total exposure time: {human_time_duration( night.exposureTime)}")
-    print (f"Total safe duration: {human_time_duration(night.getTotalSafeMinutes() * 60)}")
-    print (f"Total unsafe duration: {human_time_duration(night.getTotalUnsafeMinutes() * 60)}")
-    print (f"Number of unsafe events: {len(night.unsafe)}")
-    for unsafe in night.unsafe:
-        print(f"   {format_timestamp(unsafe.startTimestamp)}-{format_timestamp(unsafe.endTimestamp)} ({human_time_duration(unsafe.minutes() * 60)})")
+    print (f"Total exposures: {night.exposures}")
+    print (f"Exposure time: {human_time_duration( night.exposureTime)}")
+    print (f"Safe duration: {human_time_duration(night.getTotalSafeMinutes() * 60)}")
+    print (f"Unsafe duration: {human_time_duration(night.getTotalUnsafeMinutes() * 60)}")
+
+    # add observations to events
+    for obj in objects.values():                               
+        start_timestamp = obj.getStartTimestamp()
+        end_timestamp = obj.getEndTimestamp()
+        night.events.append(Event(obj.name, start_timestamp, end_timestamp))
+        
+
+    night.events.sort(key=lambda x: toDateTime(x.startTimestamp) or datetime.max)
+
+    print (f"Events: {len(night.events)}")
+    for event in night.events:
+        try:
+            start =  format_timestamp(event.startTimestamp)
+        except:
+            start = ""
+
+        try:
+            end = format_timestamp(event.endTimestamp)
+        except:
+            end = ""    
+        
+        try:
+            duration = human_time_duration(event.minutes() * 60)
+        except:
+            duration = ""
+        
+        print(f" {start}-{end}  \t{event.eventType} ({duration})")
 
     for obj in objects.values():
         obj.get_summary()
